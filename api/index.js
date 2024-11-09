529fc69495f2ee103c2747f879206403e30fa64f
wldnhariri <wildanzaeni13@gmail.com>
2024-11-09 12:54:11 +0700

    Quick fix

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+import { create, router as _router, defaults } from 'json-server';
+const server = create();
+const router = _router('db.json');
+const middlewares = defaults();
 
 server.use(middlewares);
 server.use(router);
 
-module.exports = server;
+export default server;
