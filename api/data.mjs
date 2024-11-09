69f672817fd5da6c6cde587267297e3df18f1cba
wldnhariri <wildanzaeni13@gmail.com>
2024-11-09 14:52:53 +0700

    Gunakan fs promises

--- api/data.mjs
+++ api/data.mjs
@@ -1,13 +1,15 @@
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 
-export default function handler(req, res) {
+export default async function handler(_req, res) {
     try {
-        const filePath = path.join(process.cwd(), 'db.json');
-        const fileData = fs.readFileSync(filePath, 'utf-8');
+        const filePath = path.resolve('db.json');
+        const fileData = await fs.readFile(filePath, 'utf-8');
         const data = JSON.parse(fileData);
 
         res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         res.status(200).json(data);
     } catch (error) {
         console.error("Error reading db.json:", error);
