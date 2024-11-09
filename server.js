c4c16bccb392a0f9b42cf51786c22b7e50a27d3c
wldnhariri <wildanzaeni13@gmail.com>
2024-11-09 11:32:22 +0700

    Add github gist

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const jsonServer = require('json-server');
+const axios = require('axios');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
-server.use(router);
 
-server.listen(port, () => {
-    console.log('JSON Server is running on port', port);
-});
+axios.get('https://gist.githubusercontent.com/wldnhariri/gist_id/raw/db.json')
+    .then(response => {
+        const router = jsonServer.router(response.data);
+        server.use(router);
+
+        server.listen(port, () => {
+            console.log('JSON Server is running on port', port);
+        });
+    })
+    .catch(error => {
+        console.error('Error fetching JSON data:', error);
+    });
