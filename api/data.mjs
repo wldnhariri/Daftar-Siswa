7e5c1f4ffb5b8b6411c8d05045ae24aef4670294
wldnhariri <wildanzaeni13@gmail.com>
2024-11-09 14:06:28 +0700

    Sesuaikan debugging

--- api/data.mjs
+++ api/data.mjs
@@ -2,10 +2,15 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-    const filePath = path.join(process.cwd(), 'db.json');
-    const fileData = fs.readFileSync(filePath, 'utf-8');
-    const data = JSON.parse(fileData);
+    try {
+        const filePath = path.join(process.cwd(), 'db.json');
+        const fileData = fs.readFileSync(filePath, 'utf-8');
+        const data = JSON.parse(fileData);
 
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Memungkinkan akses dari domain manapun
-    res.status(200).json(data);
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.status(200).json(data);
+    } catch (error) {
+        console.error("Error reading db.json:", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 }
