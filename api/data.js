bd6ae6cd05fbea7bdca5c9d02f2b194e1fbaa045
wldnhariri <wildanzaeni13@gmail.com>
2024-11-09 14:02:31 +0700

    Deleted

--- api/data.js
+++
@@ -1,7 +0,0 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const data = require('../db.json');
-
-export default function handler(req, res) {
-    res.status(200).json(data);
-}
